4e19ed9c7122b7dc9a4b78bf0a508f7996bc3783
srdav2 <srdav2@gmail.com>
2025-04-06 09:04:55 +1000

    Initial Commit

---
+++ Intervalz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intervalz
+//
+//  Created by stuart davis on 6/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intervalz/IntervalzApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalzApp.swift
+//  Intervalz
+//
+//  Created by stuart davis on 6/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntervalzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
